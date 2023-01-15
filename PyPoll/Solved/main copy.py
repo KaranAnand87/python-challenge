f9107a9d6d11e523550f18da4f27ba035cfb4338
Karan Anand <karananand87@gmail.com>
2023-01-14 23:52:16 -0500

    Challenge Completed

--- PyPoll/Solved/main copy.py
+++ PyPoll/Solved/main copy.py
@@ -27,9 +27,9 @@ with open(poll_csvpath, encoding="utf") as csvfile:
     
     #Sorting the list by default ascending order
     sortedlist = sorted(candidates)
-
+    arrangelist = sortedlist
     #Counting votes per candidate in most common outcome and appending them
-    countcandidate = Counter(sortedlist)
+    countcandidate = Counter(arrangelist)
     votespercandidate.append(countcandidate.most_common())
 
     #Percentage of votes per candidates upto 3 decimal points
@@ -38,7 +38,6 @@ with open(poll_csvpath, encoding="utf") as csvfile:
         second = format((item[1][1])*100/(sum(countcandidate.values())),'.3f')
         third = format((item[2][1])*100/(sum(countcandidate.values())),'.3f')
         
-   
 # Print the analysis to the terminal
 print("Election Results")
 print("-------------------------")
@@ -50,18 +49,3 @@ print(f"{votespercandidate[0][2][0]}: {third}% ({votespercandidate[0][2][1]})")
 print("-------------------------")
 print(f"Winner:  {votespercandidate[0][0][0]}")
 print("-------------------------")
-
-#Export a text file with the results
-outputfile = os.path.join("..", "Analysis", "polls_data_results.txt")
-with open(outputfile, "w") as outfile:
-
-    outfile.write("Election Results\n")
-    outfile.write("-------------------------\n")
-    outfile.write(f"Total Votes:  {votecount}\n")
-    outfile.write("-------------------------\n")
-    outfile.write(f"{votespercandidate[0][0][0]}: {first}% ({votespercandidate[0][0][1]})\n")
-    outfile.write(f"{votespercandidate[0][1][0]}: {second}% ({votespercandidate[0][1][1]})\n")
-    outfile.write(f"{votespercandidate[0][2][0]}: {third}% ({votespercandidate[0][2][1]})\n")
-    outfile.write("-------------------------\n")
-    outfile.write(f"Winner:  {votespercandidate[0][0][0]}\n")
-    outfile.write("-------------------------\n")
